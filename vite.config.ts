215fd833f27f7c170b1343c98adfa57d7c5b5eb0
na2kera <nat3mitaro@gmail.com>
2025-06-26 01:57:37 +0900

    8000番に捌くように修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://host.docker.internal:8000",
+        changeOrigin: true,
+      },
+    },
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
